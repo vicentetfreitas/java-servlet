88166bc1070f51b67a57892665eb44618fc2ca7e
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-04-01 23:38:08 -0300

    Inicializando git

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -0,0 +1,16 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+	private static List<Empresa> empresas = new ArrayList<>();
+
+	public void adiciona(Empresa empresa) {
+		Banco.empresas.add(empresa);
+	}
+
+	public List<Empresa> getEmpresas() {
+		return Banco.empresas;
+	}
+}
---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/HelloWorldServelet.java
@@ -0,0 +1,26 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class HelloWorldServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<boyd>");
+		out.println("Hellor World, Primeiro Servlets.");
+		out.println("</body>");
+		out.println("</html>");
+		System.out.println("O servlet  HelloWorldServlet foi chamado");
+	}
+
+}
